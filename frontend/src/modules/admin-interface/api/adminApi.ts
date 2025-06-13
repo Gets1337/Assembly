e180c2501d7a02a55b4ce67d46937d199c226333
Daniil <lobok.danya@list.ru>
2025-06-13 18:27:19 +0500

    fix API

--- frontend/src/modules/admin-interface/api/adminApi.ts
+++ frontend/src/modules/admin-interface/api/adminApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = `${window.location.protocol}//${window.location.host}:3000`;
-const API_URL = `${BASE_URL}/api/admin`;
+
 
 const api = axios.create({
   baseURL: BASE_URL,
